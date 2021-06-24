99ed37da6f68806180af3d61b387c0933030fa76
JaehyeonHeo <jhheo06@gmail.com>
2021-06-24 15:16:21 +0900

    WPF 설정 부분 개발완료

--- MRPApp/MRPApp/Logic/DataAccess.cs
+++ MRPApp/MRPApp/Logic/DataAccess.cs
@@ -1,14 +1,42 @@
-﻿using System;
+﻿using MRPAPP.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MRPApp.Logic
 {
     public class DataAccess
     {
-        
+        // settings 테이블에서 데이터 가져오기
+        internal static List<Settings> GetSettings()
+        {
+            List<MRPAPP.Model.Settings> settings;
+
+            using (var ctx = new MRPEntities())     // using을 사용하면 알아서 메모리 낭비를 막아준다.
+                settings = ctx.Settings.ToList();
+
+            return settings;
+        }
+
+        internal static int SetSettings(Settings item)
+        {
+            using (var ctx = new MRPEntities())
+            {
+                ctx.Settings.AddOrUpdate(item); // INSERT or update
+
+                return ctx.SaveChanges(); // commit
+            }
+        }
+
+        internal static int DelSettings(Settings item)
+        {
+            using (var ctx = new MRPEntities())
+            {
+                var obj = ctx.Settings.Find(item.BasicCode); // 검색한 실제 데이터를 찾아서,,,,
+                ctx.Settings.Remove(obj);  // DELETE (삭제)
+                return ctx.SaveChanges();
+            }
+        }
     }
 }
--- MRPApp/MRPApp/View/Setting/SettingList.xaml.cs
+++ MRPApp/MRPApp/View/Setting/SettingList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using iTextSharp.text;
+﻿using MRPAPP.Model;
+using iTextSharp.text;
 using iTextSharp.text.pdf;
 using Microsoft.Win32;
 using NPOI.HSSF.UserModel;
@@ -11,6 +12,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MRPApp.View.Setting
 {
@@ -24,11 +26,14 @@ namespace MRPApp.View.Setting
             InitializeComponent();
         }
 
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             try
             {
-                
+                LoadGridData();
+
+                InitErrorMessages();
             }
             catch (Exception ex)
             {
@@ -37,54 +42,190 @@ namespace MRPApp.View.Setting
             }
         }
 
-        private void BtnEditUser_Click(object sender, RoutedEventArgs e)
+        // 텍스트박스 옆 에러메세지 숨기는 함수
+        private void InitErrorMessages()
         {
+            LblBasicCode.Visibility = LblCodeName.Visibility = LblCodeDesc.Visibility = Visibility.Hidden;
+        }
+
+        // 그리드에 DB데이터 읽어오는 함수
+        private void LoadGridData()
+        {
+            List<MRPAPP.Model.Settings> settings = Logic.DataAccess.GetSettings();
+            this.DataContext = settings;
+        }
+
+        private async void BtnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsValidInputs() != true) return;
+
+            var setting = new MRPAPP.Model.Settings();
+            setting.BasicCode = TxtBasicCode.Text;
+            setting.CodeName = TxtCodeName.Text;
+            setting.CodeDesc = TxtCodeDesc.Text;
+            setting.RegDate = DateTime.Now;
+            setting.RegID = "MRP";
+
             try
             {
-                //NavigationService.Navigate(new EditUser());
+                var result = Logic.DataAccess.SetSettings(setting);
+                if (result == 0) // 실패했으면,,, 
+                {
+                    Commons.LOGGER.Error("데이터 입력 오류 발생");
+                    await Commons.ShowMessageAsync("오류", "데이터 입력 실패!");
+                }
+                else
+                {
+                    Commons.LOGGER.Info($"데이터 입력 성공 : {setting.BasicCode}"); //로그
+                    ClearInputs();
+                    LoadGridData();
+                }
             }
             catch (Exception ex)
             {
-                Commons.LOGGER.Error($"예외발생 BtnEditUser_Click : {ex}");
-                throw ex;
+                Commons.LOGGER.Error($"예외발생 {ex}");
             }
         }
 
-        private void BtnAddStore_Click(object sender, RoutedEventArgs e)
+        // 입력 데이터 검증 메서드
+        private bool IsValidInputs()
         {
+            var isValid = true;
+            InitErrorMessages();
+
+            if (string.IsNullOrEmpty(TxtBasicCode.Text))
+            {
+                LblBasicCode.Visibility = Visibility.Visible;
+                LblBasicCode.Text = "코드를 입력하세요";
+                isValid = false;
+            }
+            else if (Logic.DataAccess.GetSettings().Where(s =>s.BasicCode.Equals(TxtBasicCode.Text)).Count() > 0)
+            {
+                LblBasicCode.Visibility = Visibility.Visible;
+                LblBasicCode.Text = "중복 코드가 존재합니다";
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(TxtCodeName.Text))
+            {
+                LblCodeName.Visibility = Visibility.Visible;
+                LblCodeName.Text = "코드명을 입력하세요";
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        private async void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var setting = GrdData.SelectedItem as MRPAPP.Model.Settings;
+            setting.CodeName = TxtCodeName.Text;
+            setting.CodeDesc = TxtCodeDesc.Text;
+            setting.ModDate = DateTime.Now;
+            setting.ModID = "MRP";
+
             try
             {
-                //NavigationService.Navigate(new AddStore());
+                var result = Logic.DataAccess.SetSettings(setting);
+                if (result == 0) // 실패했으면,,, 
+                {
+                    Commons.LOGGER.Error("데이터 수정 오류 발생");
+                    await Commons.ShowMessageAsync("오류", "데이터 수정 실패!");
+                }
+                else
+                {
+                    Commons.LOGGER.Info($"데이터 수정 성공 : {setting.BasicCode}"); //로그
+                    ClearInputs();
+                    LoadGridData();
+                }
             }
             catch (Exception ex)
             {
-                Commons.LOGGER.Error($"예외발생 BtnAddStore_Click : {ex}");
-                throw ex;
+                Commons.LOGGER.Error($"예외발생 {ex}");
             }
         }
 
-        private void BtnEditStore_Click(object sender, RoutedEventArgs e)
+        // 텍스트박스 글 모두 지우고 입력가능하게 하는 메서드 
+        private void ClearInputs()
         {
-            if (GrdData.SelectedItem == null)
+            TxtBasicCode.IsReadOnly = false;
+            TxtBasicCode.Background = new SolidColorBrush(Colors.White);
+
+            TxtBasicCode.Text = TxtCodeName.Text = TxtCodeDesc.Text = string.Empty;
+            TxtBasicCode.Focus();
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var search = TxtSearch.Text.Trim();
+            var settings = Logic.DataAccess.GetSettings().Where(s => s.CodeName.Contains(search)).ToList();
+            this.DataContext = settings;
+        }
+
+        private void GrdData_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            try
             {
-                Commons.ShowMessageAsync("창고수정", "수정할 창고를 선택하세요");
+                var setting = GrdData.SelectedItem as MRPAPP.Model.Settings;
+                TxtBasicCode.Text = setting.BasicCode;
+                TxtCodeName.Text = setting.CodeName;
+                TxtCodeDesc.Text = setting.CodeDesc;
+
+                TxtBasicCode.IsReadOnly = true;
+                TxtBasicCode.Background = new SolidColorBrush(Colors.LightGray);
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 {ex}");
+                ClearInputs();
+            }
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            ClearInputs();
+        }
+
+        private async void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var setting = GrdData.SelectedItem as MRPAPP.Model.Settings;
+
+            if (setting == null)
+            {
+                await Commons.ShowMessageAsync("삭제", "삭제할 코드를 선택하세요");
                 return;
             }
+            else
+            {
+                try
+                {
+                    var result = Logic.DataAccess.DelSettings(setting);
+                    if (result == 0) // 실패했으면,,, 
+                    {
+                        Commons.LOGGER.Error("데이터 삭제 오류 발생");
+                        await Commons.ShowMessageAsync("오류", "데이터 삭제 실패!");
+                    }
+                    else
+                    {
+                        Commons.LOGGER.Info($"데이터 삭제 성공 : {setting.BasicCode}"); //로그
+                        ClearInputs();
+                        LoadGridData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Commons.LOGGER.Error($"예외발생 {ex}");
+                }
+            }
 
-            try
-            {
-               
-            }
-            catch (Exception ex)
-            {
-                Commons.LOGGER.Error($"예외발생 BtnEditStore_Click : {ex}");
-                throw ex;
-            }
         }
 
-        private void BtnExportExcel_Click(object sender, RoutedEventArgs e)
+        // 엔터눌리면 검색하게 하는 이벤트 
+        private void TxtSearch_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-           
+            if (e.Key == System.Windows.Input.Key.Enter)  // enter눌리면 검색 !
+                BtnSearch_Click(sender, e);
         }
+    
     }
 }
